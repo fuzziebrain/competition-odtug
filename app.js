db4d71bcc7c2241842d52d0edeb428f9cca0b6ca
Adrian Png <fuzziebrain@users.noreply.github.com>
2017-06-12 16:49:17 -0400

    Change routes

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.get('/', function(req, res, next){
   res.redirect('/pls/apex/f?p=16557:1');
 });
 
-app.use('**', proxy(
+app.use(['/i', '/pls'], proxy(
   {
     target: 'https://apex.oracle.com',
     changeOrigin: true
