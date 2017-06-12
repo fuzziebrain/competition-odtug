7fa9b7455a94fb95ba8397ba83081681861fb527
Adrian Png <adrian.png@fuzziebrain.com>
2017-06-12 15:38:02 -0700

    Clean up

--- app.js
+++ app.js
@@ -8,15 +8,10 @@ app.get('/', function(req, res, next){
   res.redirect('/pls/apex/f?p=16557:1');
 });
 
-app.use(['/i', '/pls'], proxy(
+app.use(['/i', '/pls/apex'], proxy(
   {
     target: 'https://apex.oracle.com',
-    changeOrigin: true,
-    onProxyReq: function(proxyReq, req, res) {
-      if(req.connection.encrypted && req.headers.origin) {
-        proxyReq.setHeader('origin', req.headers.origin.replace(/^https:/,'http:'));
-      }
-    }
+    changeOrigin: true
   }
 ));
 
