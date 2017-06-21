c7b3748331d5f720f2942892448ba08ba90fdc15
Adrian Png <adrian.png@fuzziebrain.com>
2017-06-20 23:22:05 -0700

    Fix for issue #1

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 const
   express = require('express'),
-  proxy = require('http-proxy-middleware');
+  proxy = require('http-proxy-middleware')
+  https = require('https')
+  fs = require('fs');
 
 var app = express();
+
+var sslEnabled = false;
+
+var sslOptions = {
+  key: fs.readFileSync('key.pem'),
+  cert: fs.readFileSync('cert.pem')
+};
 
 app.get('/', function(req, res, next){
   res.redirect('/pls/apex/f?p=16557:1');
@@ -11,8 +20,21 @@ app.get('/', function(req, res, next){
 app.use(['/i', '/pls/apex'], proxy(
   {
     target: 'https://apex.oracle.com',
-    changeOrigin: true
+    changeOrigin: true,
+    onProxyRes: function(proxyRes, req, res) {
+      if (!req.connection.encrypted && proxyRes.headers['set-cookie']) {
+        var cookies = [];
+        proxyRes.headers['set-cookie'].forEach(function(element) {
+          element = element.replace(' secure;', '');
+          cookies.push(element);
+        });
+      }
+    }
   }
 ));
 
-app.listen(process.env.PORT || 3000);
+if (sslEnabled) {
+  https.createServer(sslOptions, app).listen(process.env.PORT || 3000);
+} else {
+  app.listen(process.env.PORT || 3000);
+} 
